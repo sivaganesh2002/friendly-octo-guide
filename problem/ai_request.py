10bcf7f032f458548cc89afddf017eaa3d985f55
Aj-esh <ajeeshsunilt@gmail.com>
2025-08-09 03:56:33 +0530

    ai, system_prompted

--- problem/ai_request.py
+++ problem/ai_request.py
@@ -1,4 +1,6 @@
 from google import genai
+from google.genai import types
+
 import os
 from dotenv import load_dotenv
 
@@ -8,16 +10,41 @@ api_key = os.getenv('GEMINI_API_KEY')
 if not api_key:
     raise ValueError("GEMINI_API_KEY environment variable is not set.")
 
+system_prompt = [
+        "You are a highly skilled programming assistant. \
+        Your purpose is to help the user progress toward the correct code without giving complete solution.\
+        Your task:\
+            1. If the user is solving a problem, provide 1-5 concise, actionable hints that guide them toward the right code.\
+                * Focus on thought process, approach and concept rather than exact code.\
+                * Hints must be short, concise, clear and avoid any step-by-step solution.\
+            2. If the user shares code, review it and give concise feedback for refactoring.\
+                * Identify key issues, suggest improvement and highlight best practices.\
+                * Feedback should be high-level and  avoid rewriting any complete code.\
+        Important: \
+            * Choose exactly one direction per request (either hint or code review), never both\
+            * never write or rewrite the complete code.\
+            * keep response concise and practical.\
+            * Avoid any step-by-step solution or complete code.\
+        "
+    ]
 
-def aicall(payload:list, action:str):
+config = types.GenerateContentConfig(
+    system_instruction=system_prompt,
+    thinking_config=types.ThinkingConfig(
+        thinking_budget=0,
+    )
+)
 
-    query = '\n'.join(string for string in payload if string != '')
+def aicall(payload:list[str], action):
 
+    user_query = ["\n".join(filter(None, (s.strip() for s in payload)))]
+    
     try:
         client = genai.Client(api_key=api_key)
         response = client.models.generate_content(
             model="gemini-2.5-flash",
-            contents=query,
+            contents=user_query,
+            config=config,
         )
 
         return response.text
